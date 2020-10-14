5d59624d7026ddaf39d6e813137dde87fe138053
Julie <60452220+cwei033@users.noreply.github.com>
2020-10-13 18:28:29 -0700

    Update sketch.js

--- 2/sketch.js
+++ 2/sketch.js
@@ -1,17 +1,19 @@
 let array = [];
 
 function setup() {
-  createCanvas(600, 600);
+  createCanvas(900, 600);
   background(0);
-
+  stroke(255);
+  strokeWeight(3);
 
 }
 
 function draw() {
 
   if (mouseIsPressed) {
-    // line(mouseX, mouseY, pmouseX, pmouseY);
-    array.push([mouseX, mouseY]);
+
+    line(mouseX, mouseY, pmouseX, pmouseY);
+    // array.push([mouseX, mouseY]);
 
 
   }
@@ -22,15 +24,15 @@ function keyTyped() {
   if (key === 's') {
     console.log(`space`);
     saveCanvas('fileName', 'jpeg');
-  } else if (key === ' ') {
-    strokeWeight(5);
-    stroke(255);
-    noFill();
-    beginShape();
-    for (let i = 0; i < array.length; i++) {
-      curveVertex(array[i][0], array[i][1]);
-    }
-    endShape();
+    // } else if (key === ' ') {
+    //   strokeWeight(5);
+    //   stroke(255);
+    //   noFill();
+    //   beginShape();
+    //   for (let i = 0; i < array.length; i++) {
+    //     curveVertex(array[i][0], array[i][1]);
+    //   }
+    //   endShape();
   }
 
   return false;
